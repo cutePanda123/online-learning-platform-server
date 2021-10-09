3a68e3e73ecd511d023e98e4a90dad38d200279a
cutePanda123 <peterli1234@126.com>
2021-10-08 20:22:45 -0700

    implement message serivice

--- message-thrift-python-service/thrift/gen-py/message/message_service.py
+++ message-thrift-python-service/thrift/gen-py/message/message_service.py
@@ -1,17 +1,47 @@
+# coding: utf-8
 from message.api import MessageService
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
 
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
+sender = 'panda@163.com'
+authCode = 'testcode'
 class MessageServiceHandler:
 
-    def sendTextMessage(self, phone, message):
-        print ("send text message")
+    def sendMobileMessage(self, mobile, message):
+        print ("sendMobileMessage, mobile:"+mobile+", message:"+message)
         return True
 
     def sendEmailMessage(self, email, message):
-        print ("send email message")
-        return True
+        print ("sendEmailMessage, email:"+email+", message:"+message)
+        messageObj = MIMEText(message, "plain", "utf-8")
+        messageObj['From'] = sender
+        messageObj['To'] = email
+        messageObj['Subject'] = Header('test email', 'utf-8')
+        try:
+            smtpObj = smtplib.SMTP('smpt.163.com')
+            smtpObj.login(sender, authCode)
+            smtpObj.sendmail(sender, [email], messageObj.as_string())
+            print ("send mail success")
+            return True
+        except smtplib.SMTPException:
+            print ("send mail failed!")
+            return False
 
 
 if __name__ == '__main__':
     handler = MessageServiceHandler()
     processor = MessageService.Processor(handler)
+    transport = TSocket.TServerSocket(None, "9090")
+    tfactory = TTransport.TFramedTransportFactory()
+    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+    print ("python thrift server start")
+    server.serve()
+    print ("python thrift server exit")
