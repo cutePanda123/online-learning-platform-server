8ffa5c5b388f84050f16133e04d65dd5b04215ce
cutePanda123 <peterli1234@126.com>
2021-10-21 21:56:42 -0700

    implement redis client

--- user-edge-service/src/main/java/net/panda/user/redis/RedisConfig.java
+++ user-edge-service/src/main/java/net/panda/user/redis/RedisConfig.java
@@ -8,10 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
