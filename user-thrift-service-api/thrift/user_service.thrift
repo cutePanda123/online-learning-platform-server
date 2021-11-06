883cb0b2d3affa226004c41b20c26fab9bc9e344
cutePanda123 <peterli1234@126.com>
2021-11-06 15:52:11 -0700

    add get teacher info to user thrift api

--- user-thrift-service-api/thrift/user_service.thrift
+++ user-thrift-service-api/thrift/user_service.thrift
@@ -7,12 +7,15 @@ struct UserInfo {
     4:string realName,
     5:string mobile,
     6:string email
+    7:string intro
+    8:i32 stars
 }
 
 service UserService {
     UserInfo getUserById(1:i32 id);
     UserInfo getUserByName(1:string username);
     void registerUser(1:UserInfo userInfo);
+    UserInfo getTeacherById(1:i32 id);
 }
 
 
