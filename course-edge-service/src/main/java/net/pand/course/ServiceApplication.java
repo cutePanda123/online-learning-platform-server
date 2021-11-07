60dd19390d2a413b3df404e9bb033f71966d8939
cutePanda123 <peterli1234@126.com>
2021-11-07 09:35:17 -0800

    add service startup class

---
+++ course-edge-service/src/main/java/net/pand/course/ServiceApplication.java
@@ -0,0 +1,11 @@
+package net.pand.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+}
