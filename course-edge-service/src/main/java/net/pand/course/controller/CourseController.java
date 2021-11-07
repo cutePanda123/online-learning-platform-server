7d18ccd1e0037633f68e85ecf9fb38e2aa3e7ef6
cutePanda123 <peterli1234@126.com>
2021-11-07 09:44:03 -0800

    print user info in course controller

--- course-edge-service/src/main/java/net/pand/course/controller/CourseController.java
+++ course-edge-service/src/main/java/net/pand/course/controller/CourseController.java
@@ -3,11 +3,13 @@ package net.pand.course.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import net.panda.course.dto.CourseDTO;
 import net.panda.course.service.ICourseService;
+import net.panda.thrift.user.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -17,7 +19,10 @@ public class CourseController {
     private ICourseService courseService;
 
     @RequestMapping(value = "/courses", method = RequestMethod.GET)
-    public List<CourseDTO> listCourse() {
+    public List<CourseDTO> listCourse(HttpServletRequest request) {
+        UserDTO userDTO = (UserDTO)request.getAttribute("user");
+        System.out.println(userDTO.toString());
+        
         return courseService.listCourse();
     }
 }
