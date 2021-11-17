068cb480819792bad4cda46975a4c01990af8889
cutePanda123 <peterli1234@126.com>
2021-11-17 13:31:09 -0800

    fix CourseDTO serialization bug

--- course-dubbo-service-api/src/main/java/net/panda/course/dto/CourseDTO.java
+++ course-dubbo-service-api/src/main/java/net/panda/course/dto/CourseDTO.java
@@ -3,8 +3,10 @@ package net.panda.course.dto;
 import lombok.Data;
 import net.panda.thrift.user.dto.TeacherDTO;
 
+import java.io.Serializable;
+
 @Data
-public class CourseDTO {
+public class CourseDTO implements Serializable {
     private int id;
     private String title;
     private String description;
