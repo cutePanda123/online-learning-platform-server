8016f57142d0256e6d678dbae070e16d68d92122
cutePanda123 <peterli1234@126.com>
2021-11-17 15:22:35 -0800

    fix zuul gateway startup classnotfound issue

--- api-gateway-zuul/src/main/java/net/panda/zuul/ServiceApplication.java
+++ api-gateway-zuul/src/main/java/net/panda/zuul/ServiceApplication.java
@@ -2,8 +2,10 @@ package net.panda.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
